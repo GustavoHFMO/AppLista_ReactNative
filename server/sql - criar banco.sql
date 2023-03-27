bfc972984570d21d7cdc5aabbc122cc8f1d0c346
Gustavo Henrique Ferreira de Miranda Oliveira <guga.h.f.m.o@gmail.com>
2023-03-27 12:11:00 -0300

    Add files via upload

---
+++ server/sql - criar banco.sql
@@ -0,0 +1,11 @@
+create database bditens;
+show databases;
+use bditens;
+
+create table listaitens(
+	id int primary key auto_increment,
+	itens varchar(50) not null
+);
+
+show tables;
+describe listaitens;
